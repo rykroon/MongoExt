5765ee233ec220d8b888bf0b55657c562bea27b7
Ryan Kroon <rykroon.tech@gmail.com>
2021-11-28 13:20:26 -0500

    Added verbose methods on the Field class for each operator.

--- mongoext/fields.py
+++ mongoext/fields.py
@@ -23,20 +23,49 @@ class Field:
         return self.name
 
     def __eq__(self, value):
-        return expressions.eq(self, value)
+        return self.eq(value)
 
     def __ne__(self, value):
-        return expressions.ne(self, value)
+        return self.ne(value)
 
     def __lt__(self, value):
-        return expressions.lt(self, value)
+        return self.le(value)
 
     def __le__(self, value):
-        return expressions.lte(self, value)
+        return self.lte(value)
 
     def __gt__(self, value):
-        return expressions.gt(self, value)
+        return self.gt(value)
 
     def __ge__(self, value):
+        return self.gte(value)
+
+    def eq(self, value):
+        return expressions.eq(self, value)
+
+    def ne(self, value):
+        return expressions.ne(self, value)
+
+    def le(self, value):
+        return expressions.lt(self, value)
+
+    def lte(self, value):
+        return expressions.lte(self, value)
+
+    def gt(self, value):
+        return expressions.gt(self, value)
+
+    def gte(self, value):
         return expressions.gte(self, value)
+
+    def in_(self, value):
+        return expressions.in_(self, value)
+
+    def nin(self, value):
+        return expressions.nin(self, value)
     
+    def exists(self, value=True):
+        return expressions.exists(self, value)
+
+    def type(self, value):
+        return expressions.type(self, value)
