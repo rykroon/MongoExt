12a5f8a3a22bb2f842aae4a48620f7c7286ca962
Ryan Kroon <rykroon.tech@gmail.com>
2021-12-12 15:35:23 -0500

    Added tests for CollectionExt.

--- tests.py
+++ tests.py
@@ -1,5 +1,11 @@
 import unittest
 
+from bson import ObjectId
+from pymongo import MongoClient
+from pymongo.errors import DuplicateKeyError
+
+from mongoext.collections import CollectionExt
+from mongoext.exceptions import MissingIdException
 from mongoext.fields import Field
 
 """
@@ -207,8 +213,57 @@ class TestLogicalExpressions(unittest.TestCase):
 
 class TestCollectionExt(unittest.TestCase):
 
-    def seUp(self):
-        ...
+    def setUp(self):
+        self.client = MongoClient()
+        self.db = self.client['test_db']
+        self.db.drop_collection('test_mongoext')
+        self.coll = CollectionExt(name='test_mongoext', database=self.db)
+        
+        self.doc = {'a': 1, 'b': 2, 'c': 3}
+        self.coll.insert_one(self.doc)
+
+    def test_get_by_id(self):
+        result = self.coll.get_by_id(self.doc['_id'])
+        assert result == self.doc
+
+        obj_id = ObjectId()
+        result = self.coll.get_by_id(obj_id)
+        assert result is None
+
+    def test_delete_by_id(self):
+        result = self.coll.delete_by_id(self.doc['_id'])
+        assert result.deleted_count == 1
+
+        result = self.coll.delete_by_id(self.doc['_id'])
+        assert result.deleted_count == 0
+
+    def test_insert_document(self):
+        with self.assertRaises(DuplicateKeyError):
+            self.coll.insert_document(self.doc)
+
+        doc = {'_id': None}
+        self.coll.insert_document(doc)
+        assert doc['_id'] is not None
+
+        doc = {'_id': 123456}
+        self.coll.insert_document(doc)
+        assert doc['_id'] == 123456
+
+    def test_update_document(self):
+        self.doc['d'] = 4
+        self.coll.update_document(self.doc)
+
+        self.coll.get_by_id(self.doc['_id']) == self.doc
+
+        with self.assertRaises(MissingIdException):
+            doc_with_no_id = {}
+            self.coll.update_document(doc_with_no_id)
+
+    def test_delete_document(self):
+        result = self.coll.delete_document(self.doc)
+        assert result.deleted_count == 1
+
+        assert self.coll.get_by_id(self.doc['_id']) is None
 
 
 if __name__ == '__main__':
